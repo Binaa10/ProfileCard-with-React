918526a01d88e50f11020775d448d4345b1da337
Biniyam-Biyadge <biniyambiyadge@gmail.com>
2025-03-05 07:25:00 -0800

    inital

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img src="pictures/bini.jpg" alt="binios image" className="avatar" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Biniyam Biyadge</h1>
+      <p>
+        Full stack developer and Software Engineering student at Adama Science
+        and Technology University. When not coding and studying, I like to play
+        football.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="React" emoji="💪" color="blue" />
+      <Skill skill="HTML+CSS" emoji="💪" color="orange" />
+      <Skill skill="JavaScript" emoji="💪" color="yellow" />
+      <Skill skill="MERN" emoji="👶" color="orangered" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
